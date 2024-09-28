9e0b4e58fd9368a19bfb305a4b88065b28006aeb
itz-Naziya <meetnaziyaji@gmail.com>
2024-09-29 00:40:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,54 +98,65 @@ if page == "Page 1":
 
 # --- PAGE 2 ---
 elif page == "Page 2":
-    st.title('🎈 Machine Learning App')
-    st.info('This app builds a machine learning model')
+    st.title('🎈 Machine learning app')
 
-    with st.expander('Data'):
-        st.write('**Raw Data**')
-        df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-        st.write(df)
+st.info('This app builds a machine learning model')
 
-        st.write('**X**')
-        X_raw = df.drop('species', axis=1)
-        st.write(X_raw)
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
 
-        st.write('**y**')
-        y_raw = df.species
-        st.write(y_raw)
+  st.write('**X**')
+  X_raw=df.drop('species',axis=1)
+  X_raw
 
-    with st.expander('Data Visualization'):
-        st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+  st.write('**y**')
+  y_raw=df.species
+  y_raw
 
-    # Input features
-    with st.sidebar:
-        st.header('Input Features') 
-        island = st.selectbox('Island', ('Biscoe', 'Torgersen', 'Dream')) 
-        bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
-        bill_depth_mm = st.slider('Bill Depth (mm)', 13.1, 21.5, 17.2)
-        flipper_length_mm = st.slider('Flipper Length (mm)', 172.0, 231.0, 201.0)
-        body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
-        gender = st.selectbox('Gender', ('male', 'female'))
+with st.expander('Data Visualization'):
+  #"bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g"
+  st.scatter_chart(data=df, x= 'bill_length_mm',y='body_mass_g',color='species')
 
-    # Create a DataFrame for input features
-    data = {
-        'island': island,
-        'bill_length_mm': bill_length_mm,
-        'bill_depth_mm': bill_depth_mm,
-        'flipper_length_mm': flipper_length_mm,
-        'body_mass_g': body_mass_g,
-        'sex': gender
-        }
-    input_df = pd.DataFrame(data, index=[0])
-    input_penguins = pd.concat([input_df, X_raw], axis=0)
+#input featires
+with st.sidebar:
+  st.header('Input Features') 
+  #"bill_depth_mm","flipper_length_mm","body_mass_g","sex"
+  island=st.selectbox('Island',('Biscoe','Torgersen','Dream')) 
+  bill_length_mm=st.slider('Bill length (mm)',32.1,59.6,43.9)
+  bill_depth_mm=st.slider('Bill Depth (mm)' , 13.1,21.5,17.2)
+  flipper_length_mm=st.slider('Flipper Length (mm) ', 172.0, 231.0, 201.0)
+  body_mass_g=st.slider('Body mass (g)' , 2700.0 , 6300.0 , 4207.0 )
+  gender=st.selectbox('Gender',('male','female'))
 
-    # Data preparation: encode X
-    encode = ['island', 'sex']
-    df_penguins = pd.get_dummies(input_penguins, columns=encode)
-    input_row = df_penguins[:1]
+  #create a dataframe for input features
+  data={'island' : island , 'bill_length_mm' : bill_length_mm, ' bill_depth_mm ' : bill_depth_mm , 
+        ' flipper_length_mm' : flipper_length_mm , ' body_mass_g ' : body_mass_g ,
+        ' sex': gender }
+  input_df= pd.DataFrame(data , index = [0])
+  input_penguins = pd.concat([input_df , X_raw ],axis=0)
 
-    # Encode Y
-    target_mapper = {'Adelie': 0, 'Chinstrap': 1, 'Gentoo': 2}
-    def target_encode(val):
-        return target_mapper[val]
 
+#Data preparation
+#encode X
+encode = ['island', 'sex']
+df_penguins=pd.get_dummies(input_penguins,columns=encode)
+input_row = df_penguins[:1]
+
+#encode Y
+target_mapper = { 'Adelie' :0, 
+                 'Chinstrap': 1 , 
+                 'Gentoo' :2 }
+def target_encode(val):
+  return target_mapper[val]
+
+y=y_raw.apply(target_encode)
+y
+y_raw
+
+with st.expander('Data Preparation'):
+  st.write('**Encoded X (Input penguin)**')
+  input_row
+  st.write('**Encoded y**')
+  y
